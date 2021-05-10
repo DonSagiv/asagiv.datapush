5f2c180ef5e66ce5c8a57f2f38aee6983b72722b
Ari <DonSagiv@users.noreply.github.com>
2021-05-09 20:07:52 -0400

    Create Android Xamarin App

---
+++ asagiv.datapush.ui.mobile/asagiv.datapush.ui.mobile/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using asagiv.datapush.ui.mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace asagiv.datapush.ui.mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ asagiv.datapush.ui.mobile/asagiv.datapush.ui.mobile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+
+namespace asagiv.datapush.ui.mobile.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        #region Commands
+        public ICommand SelectFileCommand { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainPageViewModel()
+        {
+            SelectFileCommand = new DelegateCommand(async() => await SelectFileAsync());
+        }
+        #endregion
+
+        #region Methods
+        private async Task SelectFileAsync()
+        {
+            var file = await FilePicker.PickAsync();
+
+            var fileName = file.FullPath;
+        }
+        #endregion
+    }
+}
