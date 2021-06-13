dd3687205b623faa58f78ffe3dc6783f5509eab8
Ari <DonSagiv@users.noreply.github.com>
2021-06-13 06:24:06 -0400

    Test error: Payload typing.

---
+++ asagiv.datapush.common/Utilities/ByteArrayUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace asagiv.datapush.common.Utilities
+{
+    public static class ByteArrayUtils
+    {
+        public static byte[] ToByteArray(this object inputObject)
+        {
+            var bf = new BinaryFormatter();
+
+            using(var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, inputObject);
+                return ms.ToArray();
+            }
+        }
+    }
+}
