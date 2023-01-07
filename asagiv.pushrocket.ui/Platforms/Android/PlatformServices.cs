2d7d1a4d14806d1ca9970e79de8b71fc1a609b8a
Ari <DonSagiv@users.noreply.github.com>
2023-01-07 08:04:36 -0500

    Set Android Download Directory.

--- asagiv.pushrocket.ui/Platforms/Android/PlatformServices.cs
+++ asagiv.pushrocket.ui/Platforms/Android/PlatformServices.cs
@@ -5,9 +5,14 @@ namespace asagiv.pushrocket.ui
     // All the code in this file is only included on Android.
     public class PlatformServices : IPlatformServices
     {
+        private const string mainDownloadDir = "/storage/emulated/0/Download";
+
         public string GetDownloadDirectory()
         {
-            return FileSystem.Current.AppDataDirectory;
+            // var docsDirectory = Android.App.Application.Context.GetExternalFilesDir(Android.OS.Environment.DirectoryDocuments);
+            // return docsDirectory.AbsolutePath;
+
+            return mainDownloadDir;
         }
     }
 }
